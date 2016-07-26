3d7bee5052da9749b3134fea7459afa58c36b254
Muqsit Rayyan <hackhack05@gmail.com>
2016-07-26 13:31:18 -0700

    Fixed everything. Yay!

--- src/Muqsit/Main.php
+++ src/Muqsit/Main.php
@@ -1,13 +1,13 @@
 <?php
 namespace Muqsit;
 
-use pocketmine\{Server, scheduler\PluginTask, plugin\PluginBase, event\Listener, utils\Config, utils\TextFormat as TF, event\player\PlayerMoveEvent};
+use pocketmine\{Server, scheduler\PluginTask, plugin\PluginBase, event\Listener, utils\Config, utils\TextFormat as TF};
 use Muqsit\Task;
 
 class Main extends PluginBase implements Listener{
   public function onEnable(){
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    $this->getServer()->getScheduler()->scheduleRepeatingTask(new Task($this), 1);
+    $this->getServer()->getScheduler()->scheduleRepeatingTask(new Task($this), 20);
     $this->saveDefaultConfig();
   }
 }
@@ -18,11 +18,13 @@ class Task extends PluginTask{
     parent::__construct($plugin);
   }
   
-  public function onMove(PlayerMoveEvent $e){
-    $p = $e->getPlayer();
+  public function onRun($tick){
+    $pl = $this->plugin->getServer()->getOnlinePlayers();
     $cfg = $this->plugin->getConfig();
+    foreach($pl as $p){
     if(!$p->getItemInHand()->hasEnchantments()){
     $p->sendPopup(TF::GRAY."You are playing on ".TF::BOLD.$cfg->get("server-name").TF::RESET." ".$cfg->get("server-type")."\n".TF::DARK_GRAY."[".TF::LIGHT_PURPLE.count($this->plugin->getServer()->getOnlinePlayers()).TF::DARK_GRAY."/".TF::LIGHT_PURPLE.$this->plugin->getServer()->getMaxPlayers().TF::DARK_GRAY."] | ".TF::YELLOW."$".$this->plugin->getServer()->getPluginManager()->getPlugin("EconomyAPI")->myMoney($p).TF::DARK_GRAY." | ".TF::BOLD.TF::AQUA."SHOP: ".TF::RESET.TF::GREEN."shop.cosmicpe.me");
+      }
     }
   }
 }
