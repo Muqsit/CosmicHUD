6288439a2d955cb4a320d1eb277677f29f98affc
Muqsit Rayyan <hackhack05@gmail.com>
2016-07-26 12:47:22 -0700

    Update Main.php

--- src/Muqsit/Main.php
+++ src/Muqsit/Main.php
@@ -1,7 +1,7 @@
 <?php
 namespace Muqsit;
 
-use pocketmine\{Server, scheduler\PluginTask, plugin\PluginBase, event\Listener, utils\Config};
+use pocketmine\{Server, scheduler\PluginTask, plugin\PluginBase, event\Listener, utils\Config, utils\TextFormat as TF};
 use Muqsit\Task;
 
 class Main extends PluginBase implements Listener{
@@ -21,7 +21,7 @@ class Task extends PluginTask{
     $pl = $this->plugin->getServer()->getOnlinePlayers();
     $cfg = $this->plugin->getConfig();
     foreach($pl as $p){
-    $p->sendPopup(TF::GRAY."You are playing on " . $cfg->get("server-name") . $cfg->get("server-type");
+    $p->sendPopup(TF::GRAY."You are playing on ".TF::BOLD.$cfg->get("server-name").TF::RESET." ".$cfg->get("server-type")."\n".TF::DARK_GRAY."[".TF::LIGHT_PURPLE.count($this->plugin->getServer()->getOnlinePlayers()).TF::DARK_GRAY."/".TF::LIGHT_PURPLE.$this->plugin->getServer()->getMaxPlayers().TF::DARK_GRAY."] | ".TF::YELLOW."$".$this->plugin->getServer()->getPluginManager()->getPlugin("EconomyAPI")->myMoney($p).TF::DARK_GRAY." | ".TF::BOLD.TF::AQUA."SHOP: ".TF::RESET.TF::GREEN."shop.cosmicpe.me");
     }
   }
 }
